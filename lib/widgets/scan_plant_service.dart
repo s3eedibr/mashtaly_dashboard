0134780c1bf441dc040832fd349360555ee3e829
Saeed Ibrahim <111660961+s3eedibr@users.noreply.github.com>
2024-01-29 18:26:23 +0300

    Update scan_plant_service.dart

--- lib/widgets/scan_plant_service.dart
+++ lib/widgets/scan_plant_service.dart
@@ -5,7 +5,7 @@ import 'package:image_picker/image_picker.dart';
 class ScanPlantService {
   Future<http.Response> sendImageToApi(XFile imageFile) async {
     const apiUrl =
-        'https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=true&lang=en&type=kt&api-key=2b10v6ejuzlL3QNTxCILVgcXO';
+        'https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=true&lang=en&type=kt&api-key=***';
 
     try {
       var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
